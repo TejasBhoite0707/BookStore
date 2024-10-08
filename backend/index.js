b26b24870b1ff0973288c962b00931ac8fd41df6
surajpatil630304@gmail.com <tejasbhoite3006@gmail.com>
2024-10-08 14:47:46 +0530

    DONE!

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors'
 import Bookrouter from './route/book_route.js';
 import Userrouter from './route/user_route.js';
-import path from 'path'
-import exp from 'constants';
+
 dotenv.config()
 let app=express()
 let PORT=process.env.PORT || 4000;
@@ -27,15 +26,6 @@ try {
 //defining routes
 app.use('/book',Bookrouter)
 app.use('/user',Userrouter)
-//deploy
-// if(process.env.NODE_ENV==='production'){
-//     const dirPath=path.resolve();
-//     app.use(express.static('frontend/dist'));
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(dirPath,'frontend','dist','index.html'));
-//     })
-// }
-
 app.listen(PORT,()=>{
     console.log(`server is running on port-${PORT}` );
 })
